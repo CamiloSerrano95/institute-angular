7a4ef7cd8c28738dbec7996634c4cee17c463056
Camilo Serrano <casegica@gmail.com>
2020-04-18 13:07:52 -0500

    Add news components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { StudentsComponent } from './students/students.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ShowStudentComponent } from './students/show-student/show-student.component';
+import { CreateStudentComponent } from './students/create-student/create-student.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    StudentsComponent
+    StudentsComponent,
+    ShowStudentComponent,
+    CreateStudentComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
