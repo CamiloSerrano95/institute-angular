b55451f2c725d7daf45161a0d3504cff229bb858
Camilo Serrano <casegica@gmail.com>
2020-04-19 23:49:46 -0500

    Update url with environment variable

--- src/app/students/students.service.ts
+++ src/app/students/students.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Student } from './student.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,22 +13,27 @@ export class StudentsService {
   ){}
 
   getAllStudents() {
-    const url = 'http://localhost:3000/student';
+    const url = `${environment.apiUrl}/student`
     return this.http.get<Student[]>(url);
   }
 
   getStudentById(id:string) {
-    const url = `http://localhost:3000/student/${id}`;
+    const url = `${environment.apiUrl}/student/${id}`;
     return this.http.get<Student[]>(url);
   }
 
   newStudent(data: Student[]) {
-    const url = `http://localhost:3000/student`;
+    const url = `${environment.apiUrl}/student`;
     return this.http.post(url, data);
   }
 
+  updateStudent(id:string, data:any) {
+    const url = `${environment.apiUrl}/student/${id}`;
+    return this.http.put(url, data);
+  }
+
   deleteStudent(id:number) {
-    const url = `http://localhost:3000/student/${id}`;
+    const url = `${environment.apiUrl}/student/${id}`;
     return this.http.delete(url);
   }
 
